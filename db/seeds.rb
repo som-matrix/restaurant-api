49ad28b2841ee47fbb41cd4d801c74a64a0d89e1
som-matrix <swaroopsatya36@gmail.com>
2023-09-14 19:08:41 +0530

    feat: add CRUD for RESTful api

---
+++ db/seeds.rb
@@ -0,0 +1,6 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+restaurants = Restaurant.create([{ title: "The Naan House", description: "You get the best naans here" }, { title: "Longe Tamil", description: "Amma! The entire Tamil Nadu is here"}, {title: "We Kalingas", description: "Mu odia ame Odia , the best Odia food at one place"}, {title: "Parantha Factory", description: "Desi! Get delicious Parantha here"}])
